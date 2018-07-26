385d31f1bffa9467f57c9b2317c5932ef6e09d26
Jackie <jackie@ghtinc.com>
2018-07-26 23:49:30 +0800

    first commit

---
+++ cmd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$1" = "" ]; then
+	echo "./cmd.sh up|down|restart"
+elif [ "$1" = "up" ]; then
+	docker-compose up -d
+elif [ "$1" = "down" ]; then
+	docker-compose kill
+	docker-compose rm -f
+elif [ "$1" = "restart" ]; then
+	docker-compose kill
+	docker-compose rm -f
+	docker-compose up -d
+        docker-compose exec shell jupyter notebook list | cut -d'=' -f2 | cut -d' ' -f1 | grep -v Current > token
+elif [ "$1" = "exec" ]; then
+	docker-compose exec shell bash
+else
+        docker-compose ${1}
+fi
