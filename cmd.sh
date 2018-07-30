50c4c5ed660b606bc5ecbe9c90cb52ccf99b0eaa
Jackie <jackie@ghtinc.com>
2018-07-30 22:07:09 +0800

    add tensorboard & marathon function

--- cmd.sh
+++ cmd.sh
@@ -6,6 +6,8 @@ if [ "$1" = "" ]; then
 	echo "./cmd.sh up|down|restart"
 elif [ "$1" = "up" ]; then
 	docker-compose up -d
+        docker-compose exec shell jupyter notebook list | cut -d'=' -f2 | cut -d' ' -f1 | grep -v Current > token
+	docker-compose exec -d shell tensorboard --logdir './Graph'
 elif [ "$1" = "down" ]; then
 	docker-compose kill
 	docker-compose rm -f
@@ -14,6 +16,7 @@ elif [ "$1" = "restart" ]; then
 	docker-compose rm -f
 	docker-compose up -d
         docker-compose exec shell jupyter notebook list | cut -d'=' -f2 | cut -d' ' -f1 | grep -v Current > token
+	docker-compose exec -d shell tensorboard --logdir './Graph'
 elif [ "$1" = "exec" ]; then
 	docker-compose exec shell bash
 else
